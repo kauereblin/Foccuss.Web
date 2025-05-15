064bb042dd1fd4b5a3e2a97e74eec8a947ac1016
kauereblin <kaue.reblin159@gmail.com>
2025-05-14 21:03:16 -0300

    fix backend port

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const appRoutes = require('./routes/appRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 
 app.use(cors());
 app.use(express.json());
