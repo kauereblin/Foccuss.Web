218ed940bec17979a90ebd6149f945a5f5c24641
kauereblin <kaue.reblin159@gmail.com>
2025-06-13 18:20:44 -0300

    fix baseURL service

--- frontend/lib/axios.ts
+++ frontend/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "http://ec2-18-219-89-146.us-east-2.compute.amazonaws.com:3000",
 });
 
 export default api;
